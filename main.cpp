26b7d8443a5ce3ae82ee12b381a2a0a781847b80
Abhishek Chahar <abhishekchaharr@gmail.com>
2021-03-27 12:10:47 +0000

    BasicRecusrion BrushUP

--- main.cpp
+++ main.cpp
@@ -1,14 +1,45 @@
 // #include <bits/stdc++.h>
 // using namespace std;
 
-// void func(int& n){
-//   cout<<n;
-//   if(n>0){
-//     func(--n);
-//   }
+// vector<vector<char>>digitletters={
+//     {'a','b','c'},
+//     {'d','e','f'},
+//     {'g','h','i'},
+//     { 'j','k','l'   },
+//     {'m','n','o'},
+//     {'p','q','r','s'},
+//     {'t','u','v'},
+//     {'w','x','y','z'},
+// };
+// string temp;
+// vector<string>res;
+// vector<string>allPermutations(string s, string t, int i){
+
+
+
+// }
+
+// vector<string>getPermutations(string str){
+  
+//   if(str.length()==0) return res;
+
+//   // for(int i=0; i<digitletters.size();i++){
+//   //   cout<<i<<"  :";
+//   //   for(auto x: digitletters[i]){
+//   //     cout<<x<<" ";
+//   //   }
+//   //   cout<<endl;
+//   // }
+
+//   allPermutations(str, temp, i );
+
+
+
+//   return res;
 // }
 
 // int main() {
-//   int x=10;
-//   func(x);
+//   string str;
+//   cin>>str;
+//   getPermutations(str);
 // }
